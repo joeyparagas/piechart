1dad8e291497bda9454906fc6562928b2a3d2098
joeyparagasgit config --global user.name joeyparagasgit config --global user.name joeyparagas <joey.paragas@gmail.com>
2020-04-21 13:51:24 -0700

    Working pie chart with hover effects on modern browsers

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-$('body').on('click', function (e) {
-  let x = $(e.target);
-  console.log(x);
-})
+// Hover on each pie slice, remove other visibility class 
+// Add visibility to selected slice 
+$('.pie').hover(
+  function () {
+    const x = $(this).parent();
+    for (let i = 1; i < 11; i++) {
+      // Check if ID matches selected slice
+      if (x[0].id == 'pieSlice' + i) {
+        $('.innerCircle div').removeClass('is-visible');
+        $('.content' + i).addClass('is-visible');
+        $(this).children('.sub-title').addClass('is-visible');
+      }
+    }
+  }, function () {
+    $('.sub-title').removeClass('is-visible');
+  }
+);
+
+// Remove all visibility & add to main title upon exiting pie
+$('.container').hover(
+  function () {
+    $('.innerCircle div').removeClass('is-visible')
+    $('.content-title').addClass('is-visible')
+  }
+)
